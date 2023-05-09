ca11976f68c17c14f5cff2e331a159cf297a8b71
Nickrts <n.orsolits@gmail.com>
2023-05-09 15:36:41 +0200

    Projektdateien hinzufügen.

---
+++ primzahlen_tester/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primzahlen_tester
+{
+    public class Prim
+    {
+     public void mcprim(string s)
+        {
+            String[] Separator = { "," };
+            string[] ary = s.Split(",");
+            string[] sary = new string[ary.Length];
+
+
+            for(int i = 0; i < ary.Length; i++)
+            {
+                try
+                {
+                    sary[i] = ary[i];
+                }
+                catch
+                {
+
+                }
+            }
+
+        }
+    }
+}
---
+++ primzahlen_tester/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+namespace primzahlen_tester
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //C:\Users\norso\source\repos\primzahlen_tester\primzahlen_tester
+            //SEW2-Uebung-13-Files.csv
+        StreamReader sr = new StreamReader("SEW2-Uebung-13-Files.csv");
+
+
+        string s = sr.ReadToEnd();
+        Console.WriteLine(s);
+            
+        
+
+
+
+
+        }
+    }
+}
